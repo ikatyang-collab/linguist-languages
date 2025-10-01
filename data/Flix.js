9e2f2d695f19f0b1f49c919c9d0db8dbcc75341e
fisker <172584+fisker@users.noreply.github.com>
2025-10-01 02:06:02 +0000

    feat: update data

---
+++ data/Flix.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'Flix',
+  type: 'programming',
+  aceMode: 'flix',
+  extensions: ['.flix'],
+  languageId: 800935960,
+  tmScope: 'source.flix',
+  color: '#d44a45',
+}
---
+++ data/KoLMafia_20_ASH.d.ts
@@ -0,0 +1,10 @@
+declare const _: {
+  name: 'KoLMafia ASH'
+  type: 'programming'
+  aceMode: 'text'
+  extensions: ['.ash']
+  languageId: 852099832
+  tmScope: 'source.ash'
+  color: '#B9D9B9'
+}
+export default _
