9344755f1b621c3a953f8f1bd2b0c70c1240ab58
fisker Cheung <lionkay@gmail.com>
2026-01-01 21:45:05 +0800

    Revert "Use released version instead (#317)"
    This reverts commit b65f1fc72f613143ec68571029ad0945e9923d20.

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,16 @@
 import fs from 'node:fs/promises'
-import assert from 'node:assert/strict'
 import { parseArgs } from 'node:util'
 import styleText from 'node-style-text'
 import { outdent } from 'outdent'
 import {
   generateFiles,
   writeFile,
+  fetchText,
   readFile,
   PROJECT_ROOT,
-  downloadText,
+  DATA_URLS,
   DATA_FILE,
+  downloadData,
 } from './build/index.js'
 
 const { values: options } = parseArgs({
@@ -19,27 +20,8 @@ const { values: options } = parseArgs({
   },
 })
 
-let data
-if (options.update) {
-  const text = await downloadText({
-    url: 'https://api.github.com/repos/github-linguist/linguist/releases/latest',
-    cacheFile: new URL('./.cache/releases-latest.json', PROJECT_ROOT),
-  })
-  const { tag_name: tagName } = JSON.parse(text)
-
-  assert.ok(tagName.startsWith('v'))
-  const version = tagName.slice(1)
-
-  data = await downloadText({
-    url: `https://raw.githubusercontent.com/github-linguist/linguist/refs/tags/${tagName}/lib/linguist/languages.yml`,
-    cacheFile: new URL(`./.cache/${tagName}-languages.yml`, PROJECT_ROOT),
-  })
-  await writeFile(DATA_FILE, `# Version: ${version}\n${data}`, {
-    format: false,
-  })
-} else {
-  data = await readFile(DATA_FILE)
-}
+let data =
+  (options.update ? '' : await readFile(DATA_FILE)) || (await downloadData())
 
 if (!options.dry) {
   await fs.rm(new URL('./data/', PROJECT_ROOT), {
---
+++ scripts/build/download-data.js
@@ -0,0 +1,21 @@
+import { outdent } from 'outdent'
+import { PROJECT_ROOT, DATA_URLS, DATA_FILE } from './constants.js'
+import { writeFile, fetchText, readFile } from './utilities.js'
+
+async function downloadData() {
+  const data = await Promise.any(
+    DATA_URLS.map(async url => {
+      const text = await fetchText(url)
+      return outdent`
+        # Data downloaded at ${new Date().toISOString()}
+        ${text}
+      `
+    }),
+  )
+
+  await writeFile(DATA_FILE, data, { format: false })
+
+  return data
+}
+
+export default downloadData
