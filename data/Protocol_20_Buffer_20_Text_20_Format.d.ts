f886618d98419dffe8a7262e1a05388464b84f20
fisker <172584+fisker@users.noreply.github.com>
2026-04-05 02:31:27 +0000

    Update Linguist to v9.5.0
    https://github.com/github-linguist/linguist/releases/tag/v9.5.0

--- data/Protocol_20_Buffer_20_Text_20_Format.d.ts
+++ data/Protocol_20_Buffer_20_Text_20_Format.d.ts
@@ -2,7 +2,7 @@ declare const _: {
   readonly name: 'Protocol Buffer Text Format'
   readonly type: 'data'
   readonly aceMode: 'text'
-  readonly extensions: readonly ['.textproto', '.pbt', '.pbtxt']
+  readonly extensions: readonly ['.textproto', '.pbt', '.pbtxt', '.txtpb']
   readonly languageId: 436568854
   readonly tmScope: 'source.textproto'
   readonly aliases: readonly ['text proto', 'protobuf text format']
--- data/TSX.js
+++ data/TSX.js
@@ -5,6 +5,7 @@ export default {
   extensions: ['.tsx'],
   languageId: 94901924,
   tmScope: 'source.tsx',
+  aliases: ['typescriptreact'],
   codemirrorMode: 'jsx',
   codemirrorMimeType: 'text/typescript-jsx',
   color: '#3178c6',
